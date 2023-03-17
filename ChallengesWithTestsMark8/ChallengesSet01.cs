356b6264c8651c76c7b9b334827c587ab4bb93c0
Victoria Pham <victoriapham52@gmail.com>
2023-03-17 12:17:51 -0700

    Commit Weekly Challenge

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -6,44 +6,80 @@ namespace ChallengesWithTestsMark8
     {
         public bool AreTwoNumbersTheSame(int num1, int num2)
         {
-            if(num1 == num2)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+            //if(num1 == num2)
+            //{
+            //    return true;
+            //}
+            //else
+            //{
+            //    return false;
+            //}
+
+            //ternary
+            return (num1 == num2 ? true : false);
         }
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            var subtractAnswer = minuend - subtrahend;
+
+            return subtractAnswer;
+
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            return (number1 + number2);
+
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            //if (number1 < number2)
+            //{
+            //    return number1;
+            //}
+            //else
+            //{
+            //    return number2;
+            //}
+
+            //ternary
+            return (number1 < number2 ? number1 : number2);
+
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            return (factor1 * factor2);
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+
+            if (nameOfPerson != "")
+            {
+                return $"Hello, {nameOfPerson}!";
+            }
+
+            else
+            {
+                return "Hello!";
+
+            }
+
+          
         }
 
         public string GetHey()
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            return "HEY!";
+
         }
     }
 }
