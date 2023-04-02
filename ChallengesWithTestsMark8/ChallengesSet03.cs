3241d301b822d984df79637e77f9b53040a6846e
Victoria Pham <victoriapham52@gmail.com>
2023-04-02 16:42:18 -0700

    Weekly Challenge 3 commit

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,48 +8,106 @@ namespace ChallengesWithTestsMark8
     {
         public bool ArrayContainsAFalse(bool[] vals)
         {
-            throw new NotImplementedException();
-            //for (int i = 0; i < vals.Length; i++ )
+
+            //if (vals == null)
+            //{
+            //    return false;
+            //}
+
+
+            //foreach (var item in vals)
+            //{
+            //    if (item == false)
+            //    {
+            //        return true;
+            //    }                            
+
+            //}; return false;
+
+            return vals.Contains(false) ? true : false; 
+
         }
 
         public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
         {
-            throw new NotImplementedException();
+
+            if (numbers == null)
+            {
+                return false;
+            }
+
+            var sumOfOdds = numbers.Where(x => x % 2 != 0).Sum();
+
+            foreach (var item in numbers)
+            {
+
+                if (sumOfOdds % 2 != 0)
+                { 
+                   
+                    return true;
+                }
+
+            }return false;
+            
         }
 
         public bool PasswordContainsUpperLowerAndNumber(string password)
         {
-            throw new NotImplementedException();
+            return (password.Any(char.IsUpper) && password.Any(char.IsLower) && password.Any(char.IsNumber));     
+
         }
 
         public char GetFirstLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            return val.First();
         }
 
         public char GetLastLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            return val.Last();
         }
 
         public decimal Divide(decimal dividend, decimal divisor)
         {
-            throw new NotImplementedException();
+            if (divisor == 0)
+            {
+                return 0;
+            }
+
+            return dividend / divisor;
         }
 
         public int LastMinusFirst(int[] nums)
         {
-            throw new NotImplementedException();
+            return nums.Last() - nums.First();
         }
 
         public int[] GetOddsBelow100()
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            var odds = new List<int>();
+
+            
+            for (int i = 1; i < 100; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    odds.Add(i);
+                }
+            }
+
+            return odds.ToArray();
+
+            // return Enumerable.Range(0, 100).Where(x => x % 2 != 0).ToArray();
+
         }
 
         public void ChangeAllElementsToUppercase(string[] words)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = words[i].ToUpper();
+            }
         }
     }
 }
