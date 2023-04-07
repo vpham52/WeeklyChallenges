3684edafca5af78ed5a65bde6f9c0b83cbb104cf
Victoria Pham <victoriapham52@gmail.com>
2023-04-07 13:23:07 -0700

    Weekly challenge 4 commit

--- ChallengesWithTestsMark8/ChallengesSet04.cs
+++ ChallengesWithTestsMark8/ChallengesSet04.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -6,47 +8,174 @@ namespace ChallengesWithTestsMark8
     {
         public int AddEvenSubtractOdd(int[] numbers)
         {
-            throw new NotImplementedException();
+            var equals = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    equals += numbers[i];
+                }
+
+                if (numbers[i] % 2 != 0)
+                {
+                    equals -= numbers[i];
+                }
+            }
+
+            return equals;
+
+            /*using foreach loop
+            foreach (var num in numbers)
+            {
+                if (num % 2 == 0)
+                {
+                    equals += num;
+                }
+
+                if (num % 2 != 0)
+                {
+                    equals -= num)
+                }
+
+            }*/
+
+            /* using linq
+            numbers.Where(x => x % 2 == 0).Sum() - numbers.Where(x => x % 2 != 0).Sum();
+            */
+
+
         }
 
         public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
         {
-            throw new NotImplementedException();
+            /*var list = new List<int>() {str1.Length, str2.Length, str3.Length, str4.Length };
+
+            return list.Min();
+            */
+
+
+            return new List<string> { str1, str2, str3, str4 }.Min(x => x.Length);
         }
 
         public int GetSmallestNumber(int number1, int number2, int number3, int number4)
         {
-            throw new NotImplementedException();
+            //var list = new List<int>() { number1, number2, number3, number4 };
+            //return list.Min();
+
+            return new List<int> { number1, number2, number3, number4 }.Min();
         }
 
         public void ChangeBusinessNameTo_TrueCoders(Business biz)
         {
-            throw new NotImplementedException();
+            biz.Name = "TrueCoders";
         }
 
         public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)
         {
-            throw new NotImplementedException();
+            if (sideLength1 + sideLength2 > sideLength3 &&
+                sideLength1 + sideLength3 > sideLength2 &&
+                sideLength2 + sideLength3 > sideLength1)
+            {
+                return true;
+            }
+
+            else return false;
+
+
         }
 
         public bool IsStringANumber(string input)
         {
-            throw new NotImplementedException();
+            return double.TryParse(input, out var number);
+                      
         }
 
         public bool MajorityOfElementsInArrayAreNull(object[] objs)
         {
-            throw new NotImplementedException();
+            var countNull = objs.Count(x => x == null);
+            var countOther = objs.Count(x => x != null);
+
+            if (countNull > countOther)
+            {
+                return true;
+            }
+            else return false;
+
+      
+             
+
+            /*var amount = objs.Length;
+            var majority = (amount / 2) + 1;
+
+            var count = 0;
+
+            foreach (var obj in objs)
+            {
+                if (obj == null)
+                {
+                    count++;
+                }
+                
+            }
+            return count >= majority;*/
+
+            //return Convert.ToDouble(objs.Where(x => x == null).Count()) / objs.Length > .5;
+
         }
 
         public double AverageEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            double sum = 0;
+            int count = 0;
+
+            if (numbers == null)
+            return 0;
+
+            foreach (var num in numbers)
+            {
+                if (num % 2 == 0 )
+                {
+                    sum += num;
+                    count++;
+                }
+            }
+
+            if (count > 0)
+            {
+                return sum / count;
+            }
+
+            else
+            {
+                return 0;
+            }
+
+            // return (numbers?.Where(x=> x%2 == 0).Count() > 0) ? numbers.Where(x => x %2 == 0).Average(); 0;
         }
 
         public int Factorial(int number)
         {
-            throw new NotImplementedException();
+            var result = 1;
+
+            if (number == 0)
+            {
+                return 1;
+            }
+
+            if (number < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            for (var i = number; i > 0; i--)
+            {
+                result *= i;
+            }
+         
+            return result;
+
+            //linq return (number == 0) ? 1 : Enumerable.Range(1, number).Aggregate
         }
     }
 }
